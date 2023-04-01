e8242a77de782a2906b6e4b7b62264507e10a5d2
Baptiste Longy <baptiste.longy@gmail.com>
2023-04-01 15:35:05 +0200

    feat: block update from other users

--- buttonHandlers/update.js
+++ buttonHandlers/update.js
@@ -1,22 +1,30 @@
 const { ActionRowBuilder, ModalBuilder, TextInputStyle, TextInputBuilder } = require('discord.js');
 
+const adminRoleId = process.env.DISCORD_SERVER_ADMIN_ROLE;
+
 async function update(interaction) {
-    const updateModal = new ModalBuilder()
-        .setCustomId('updateModal')
-        .setTitle('Modification');
+    const adminRole = interaction.member.roles.cache.get(adminRoleId);
 
-    const newDescriptionInput = new TextInputBuilder()
-        .setCustomId('newDescriptionInput')
-        .setLabel('Nouvelle description')
-        .setStyle(TextInputStyle.Paragraph)
-        .setValue(interaction.message.embeds[0].description);
+    if (adminRole === undefined && interaction.member.toString() !== interaction.message.content.split(' ').pop()) {
+        await interaction.reply({ content: 'Oh Bebou... tu peux pas modifier le raid de quelqu\'un d\'autre...', ephemeral: true });
+    } else {
+        const updateModal = new ModalBuilder()
+            .setCustomId('updateModal')
+            .setTitle('Modification');
 
-    const newDescriptionRow = new ActionRowBuilder()
-        .addComponents(newDescriptionInput);
+        const newDescriptionInput = new TextInputBuilder()
+            .setCustomId('newDescriptionInput')
+            .setLabel('Nouvelle description')
+            .setStyle(TextInputStyle.Paragraph)
+            .setValue(interaction.message.embeds[0].description);
 
-    updateModal.addComponents(newDescriptionRow);
+        const newDescriptionRow = new ActionRowBuilder()
+            .addComponents(newDescriptionInput);
 
-    await interaction.showModal(updateModal);
+        updateModal.addComponents(newDescriptionRow);
+
+        await interaction.showModal(updateModal);
+    }
 }
 
 module.exports = {
