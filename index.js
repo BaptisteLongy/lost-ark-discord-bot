69a162dca05b03cfa393627253e78a6cb176c766
Baptiste Longy <baptiste.longy@gmail.com>
2023-04-02 09:56:18 +0200

    chore: clean up

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ dotenv.config();
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { createCollection } = require('./tools/createCollection.js');
 
-// For when I'll reinstate roster sniffer eventually
-// Google Vision
-// const vision = require('@google-cloud/vision');
-// const vision_client = new vision.ImageAnnotatorClient();
-
 const token = process.env.LOST_ARK_DISCORD_BOT_TOKEN;
 
 // Create a new client instance
@@ -130,63 +125,6 @@ client.on(Events.InteractionCreate, async interaction => {
 		}
 	}
 });
-
-// For when I'll reinstate roster sniffer eventually
-// client.on('messageCreate', async (message) => {
-//     if (message.author.bot) return;
-
-//     if (message.channel.id === '895331467519983698') {
-//         // run code here
-//         console.log('Message detected');
-//         // message.attachments[0].value.attachment
-//         const attachment = message.attachments.at(0)
-//         const [result] = await vision_client.textDetection(attachment.attachment);
-//         const detections = result.textAnnotations;
-//         // console.log('Text:');
-//         const detectionsArray = detections[0].description.split('\n');
-//         // debugger
-
-//         const allCharacters = [];
-
-//         while (detectionsArray.length > 0) {
-//             const workingArray = [];
-
-//             // Init
-//             workingArray.push(detectionsArray.shift());
-//             workingArray.push(detectionsArray.shift());
-//             workingArray.push(detectionsArray.shift());
-
-//             try {
-//                 while (workingArray[workingArray.length - 1] && !workingArray[workingArray.length - 1].includes('N. ')) {
-//                     workingArray.shift();
-//                     workingArray.push(detectionsArray.shift());
-//                 }
-//             }
-//             catch (e) {
-//                 debugger
-//             }
-
-//             if (workingArray[2] && workingArray[2].includes('N. ')) {
-//                 const currentCharacter = {
-//                     class: workingArray[0],
-//                     name: workingArray[1],
-//                     ilvl: workingArray[2].substring(workingArray[2].lastIndexOf(' ') + 1),
-//                 };
-
-
-//                 allCharacters.push(currentCharacter);
-//             }
-//         }
-//         // debugger
-
-//         message.channel.send(`Liste des persos de ${message.author.username}`);
-//         allCharacters.forEach(character => {
-//             message.channel.send(`Nom : ${character.name} - Classe : ${character.class} - Item Level : ${character.ilvl}`)
-//         })
-
-//         // detections.forEach(text => console.log(text));
-//     }
-// });
 
 // Log in to Discord with your client's token
 client.login(token);
