77e838c09fc220c5cccec02fdd2e525476cdd7b9
Baptiste Longy <baptiste.longy@gmail.com>
2023-08-17 09:45:22 +0200

    chore: share getRandomInt

--- commands/de.js
+++ commands/de.js
@@ -1,16 +1,11 @@
 const { SlashCommandBuilder } = require('discord.js');
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
+const { getRandomInt } = require('../tools/getRandomInt.js');
 
 const data = new SlashCommandBuilder()
     .setName('dé')
     .setDescription('Lance un D100');
 
 async function execute(interaction) {
-
-
         await interaction.reply({
             content: `${interaction.member} fait un **${getRandomInt(100)}**`,
         });
