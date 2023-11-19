c332ae1fa7fc3d89419f35f2e28ea946b463bf76
Baptiste Longy <baptiste.longy@gmail.com>
2023-11-20 00:08:32 +0100

    feat: keep vocal channels close

--- voiceCreatorManager/newVoiceChannel.js
+++ voiceCreatorManager/newVoiceChannel.js
@@ -4,7 +4,16 @@ async function createNewVoiceChannelAndMoveUser(newState) {
     await newState.member.fetch(true);
     await newState.member.user.fetch(true);
     const userName = newState.member.displayName ? newState.member.displayName : newState.member.user.displayName ? newState.member.user.displayName : newState.member.user.username;
-    await newState.guild.channels.create({
+    const channelList = newState.channel.parent.children.cache;
+    let creationPosition = 0;
+    for (const value of channelList.values()) {
+        if (value.id === process.env.DISCORD_VOICE_CHANNEL_CREATOR_ID) {
+            creationPosition = Math.max(creationPosition, value.position + 1);
+        } else if (value.name.startsWith('Chez ')) {
+            creationPosition = Math.max(creationPosition, value.position + 1);
+        }
+    }
+    const newChannel = await newState.guild.channels.create({
         name: `Chez ${userName}`,
         type: ChannelType.GuildVoice,
         parent: newState.channel.parent ? newState.channel.parent : undefined,
@@ -18,9 +27,9 @@ async function createNewVoiceChannelAndMoveUser(newState) {
                 allow: [PermissionFlagsBits.ManageChannels, PermissionFlagsBits.Connect],
             },
         ],
-    }).then(async (newChannel) => {
-        await newState.member.voice.setChannel(newChannel.id);
     });
+    await newChannel.edit({ position: creationPosition });
+    await newState.member.voice.setChannel(newChannel.id);
 }
 
 module.exports = {
