1002dc16b55a3db862d92fbfe6791fffe8d01b13
Baptiste Longy <baptiste.longy@gmail.com>
2024-03-06 17:21:10 +0100

    refactor: use the registration manager for simple interactions

--- buttonHandlers/registerBench.js
+++ buttonHandlers/registerBench.js
@@ -1,31 +1,10 @@
-const { RaidMessage } = require('../tools/message/RaidMessage.js');
-const logger = require ('../tools/logger.js');
+const { RaidRegistrationManager } = require('../tools/RaidRegistrationManager.js');
 
 async function registerBench(interaction) {
     await interaction.deferUpdate();
 
-    // Parse the message into a RaidMessage
-    const raidMessage = new RaidMessage();
-
-    const initialMessage = await interaction.message.fetchReference();
-
-    raidMessage.initWithEmbed(initialMessage.embeds[0]);
-
-    // Update the RaidMessage
-    raidMessage.update(interaction.member, 'Banc de touche');
-
-    // Generate the new embed
-    const newEmbed = raidMessage.generateEmbed();
-
-    // Send the new embed
-    await initialMessage.edit({ embeds: [newEmbed] });
-
-    // Add the member to the thread
-    initialMessage.channel.members.add(interaction.member);
-
-    await interaction.editReply({ content: '**Tu es ajouté sur le banc de touche !**\nTu peux encore changer bien sûr' });
-
-    logger.logAction(interaction, `Id: ${initialMessage.id} : ${interaction.member.displayName} s'est ajouté au raid ${raidMessage.raid.value} - Role : Banc de touche`);
+    const registrationManager = new RaidRegistrationManager(interaction, interaction.message.mentions.channels.size > 0, interaction.channel.isThread ? interaction.channel : undefined);
+    registrationManager.registerAs('Banc de touche');
 }
 
 module.exports = {
