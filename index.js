37dcb5a5dbc707d8c960ccaf449f7a377a5a3720
Baptiste Longy <baptiste.longy@gmail.com>
2024-04-02 18:09:11 +0200

    Revert "Merge branch 'whisp-for-ratik-merchant'"
    This reverts commit f55c8ae333b22b713cc5dd472a27c75bd85bde76, reversing
    changes made to 01dcb2f1fc39195003a16b66710884501b76150b.

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ const { initCronJobs } = require('./cronJobs/initCronJobs.js');
 const token = process.env.LOST_ARK_DISCORD_BOT_TOKEN;
 
 // Global var to store which cards have been pinged already
-global.recentlyPingedCardsForArcturus = [];
-global.recentlyPingedCardsForRatik = [];
+global.recentlyPingedCards = [];
 
 // Create a new client instance
 const client = new Client({
