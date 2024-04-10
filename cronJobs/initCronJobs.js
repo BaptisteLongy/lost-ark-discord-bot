4892e9a8261855c844f11fd77553627b992de8dd
Baptiste Longy <baptiste.longy@gmail.com>
2024-04-10 11:04:52 +0200

    Revert "Revert "Merge branch 'lostmerchants-scrape'""
    This reverts commit c969b10cf01612ae71b7870ab7c7d3afc716185e.

--- cronJobs/initCronJobs.js
+++ cronJobs/initCronJobs.js
@@ -2,6 +2,8 @@ const { todoOfficersJob } = require('./todoOfficersJob.js');
 const { reminderJob } = require('./reminderJob.js');
 // const { reminderGVGRegistration } = require('./reminderGVGRegistration.js');
 const { reminderEventEndDate } = require('./reminderEventEndDate.js');
+const { checkLegendaryCards } = require('./checkLegendaryCards.js');
+const { vocalCleanUp } = require('./vocalCleanUp.js');
 
 function initCronJobs(client) {
     todoOfficersJob(client);
@@ -9,6 +11,8 @@ function initCronJobs(client) {
     // GVG disabled for now
     // reminderGVGRegistration(client);
     reminderEventEndDate(client);
+    checkLegendaryCards(client);
+    vocalCleanUp(client);
 }
 
 module.exports = {
