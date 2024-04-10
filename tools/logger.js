b56b6fe02f4027c60c65d08e4e823e0d29d67c70
Baptiste Longy <baptiste.longy@gmail.com>
2024-04-10 11:03:00 +0200

    feat: add debug feat to logger

--- tools/logger.js
+++ tools/logger.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const logChannelId = process.env.DISCORD_LOG_CHANNEL;
+const logLevel = process.env.DISCORD_BOT_LOG_LEVEL;
 
 function logAction(interaction, message) {
     interaction.guild.channels.fetch(logChannelId)
@@ -38,8 +39,19 @@ function logMessage(guild, message) {
         .catch(console.error);
 }
 
+function logDebugInfo(guild, message) {
+    if (logLevel === 'DEBUG') {
+        guild.channels.fetch(logChannelId)
+            .then(channel => {
+                channel.send(`DEBUG INFO\n${message}`);
+            })
+            .catch(console.error);
+    }
+}
+
 module.exports = {
     logAction,
     logError,
     logMessage,
+    logDebugInfo,
 };
