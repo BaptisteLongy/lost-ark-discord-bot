2a5a7136ae88ff7be341fdd28ef528b5f1c5b3eb
Baptiste Longy <baptiste.longy@gmail.com>
2024-04-29 10:50:46 +0200

    fix: wrong message on friday

--- cronJobs/reminderGoldIsland.js
+++ cronJobs/reminderGoldIsland.js
@@ -10,11 +10,11 @@ function reminderGoldIslandIsToday(client) {
         async function() {
             try {
                 const goldIslandDateArray = fs.readFileSync(process.env.DISCORD_BOT_GOLD_ISLAND_CONFIG_FILE).toString().split('\n');
-                const today = new Date().toDateString();
+                const today = new Date();
 
                 for (const i in goldIslandDateArray) {
                     const loopDate = new Date(goldIslandDateArray[i]).toDateString();
-                    if (loopDate === today) {
+                    if (loopDate === today.toDateString()) {
                         const notificationChannel = await client.channels.cache.get(process.env.DISCORD_SERVER_NOTIFICATION_CHANNEL);
                         if (today.getDay() === 4 || today.getDay() === 6) {
                             notificationChannel.send('@here **⚠️ AUJOURD\'HUI : DOUBLE ILE A GOLD + CHAOS GATE ⚠️**\nIl est préférable de faire double chaos gate');
@@ -35,21 +35,20 @@ function reminderGoldIslandIsToday(client) {
 
 function reminderGoldIslandIsTomorrowOnWeekDays(client) {
     new CronJob(
-        // '0 30 10 * * 1-5',
+        '0 30 10 * * 1-5',
         // For Dev - every 5 seconds
-        '*/5 * * * * *',
+        // '*/5 * * * * *',
         async function() {
             try {
                 const goldIslandDateArray = fs.readFileSync(process.env.DISCORD_BOT_GOLD_ISLAND_CONFIG_FILE).toString().split('\n');
-                let tomorrow = new Date();
+                const tomorrow = new Date();
                 tomorrow.setDate(tomorrow.getDate() + 1);
-                tomorrow = tomorrow.toDateString();
 
                 for (const i in goldIslandDateArray) {
                     const loopDate = new Date(goldIslandDateArray[i]).toDateString();
-                    if (loopDate === tomorrow) {
+                    if (loopDate === tomorrow.toDateString()) {
                         const notificationChannel = await client.channels.cache.get(process.env.DISCORD_SERVER_NOTIFICATION_CHANNEL);
-                        if (tomorrow.getDay() === 4) {
+                        if (tomorrow.getDay() === 4 || tomorrow.getDay() === 6) {
                             notificationChannel.send('@here **⚠️ DEMAIN : ILE A GOLD + CHAOS GATE ⚠️**\nDouble ile à gold demain, mais également chaos gate.\nIl est préférable de faire double chaos gate');
                         } else {
                             notificationChannel.send('@here **⚠️ DEMAIN : ILE A GOLD ⚠️**\nDouble ile à gold demain, alors ne faites pas votre ile d\'aventure aujourd\'hui');
@@ -74,19 +73,14 @@ function reminderGoldIslandIsTomorrowOnWeekEnds(client) {
         async function() {
             try {
                 const goldIslandDateArray = fs.readFileSync(process.env.DISCORD_BOT_GOLD_ISLAND_CONFIG_FILE).toString().split('\n');
-                let tomorrow = new Date();
-                tomorrow.setDate(tomorrow.getDate() + 1);
-                tomorrow = tomorrow.toDateString();
+                const tomorrow = new Date();
+                tomorrow.setDate(tomorrow.getDate() + 5);
 
                 for (const i in goldIslandDateArray) {
                     const loopDate = new Date(goldIslandDateArray[i]).toDateString();
-                    if (loopDate === tomorrow) {
+                    if (loopDate === tomorrow.toDateString()) {
                         const notificationChannel = await client.channels.cache.get(process.env.DISCORD_SERVER_NOTIFICATION_CHANNEL);
-                        if (tomorrow.getDay() === 6) {
-                            notificationChannel.send('@here **⚠️ DEMAIN : ILE A GOLD + CHAOS GATE ⚠️**\nDouble ile à gold demain, mais également chaos gate.\nIl est préférable de faire double chaos gate');
-                        } else {
-                            notificationChannel.send('@here **⚠️ DEMAIN : ILE A GOLD ⚠️**\nDouble ile à gold demain, alors ne faites pas votre ile d\'aventure aujourd\'hui');
-                        }
+                        notificationChannel.send('@here **⚠️ DEMAIN : ILE A GOLD ⚠️**\nDouble ile à gold demain, alors ne faites pas votre ile d\'aventure aujourd\'hui');
                     }
                 }
             } catch (error) {
