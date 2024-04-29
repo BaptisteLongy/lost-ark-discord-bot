75f0c7cfa561a0d885a91d6cf96d2db7bd7f1ba6
Baptiste Longy <baptiste.longy@gmail.com>
2024-04-29 17:48:28 +0200

    feat: add info for gold island on wednesday

--- cronJobs/reminderGoldIsland.js
+++ cronJobs/reminderGoldIsland.js
@@ -18,6 +18,8 @@ function reminderGoldIslandIsToday(client) {
                         const notificationChannel = await client.channels.cache.get(process.env.DISCORD_SERVER_NOTIFICATION_CHANNEL);
                         if (today.getDay() === 4 || today.getDay() === 6) {
                             notificationChannel.send('@here **⚠️ AUJOURD\'HUI : DOUBLE ILE A GOLD + CHAOS GATE ⚠️**\nIl est préférable de faire double chaos gate');
+                        } else if (today.getDay() === 3) {
+                            notificationChannel.send('@here **⚠️ AUJOURD\'HUI : ILE A GOLD + MAINTENANCE ⚠️**\nRIP si les serveurs sont toujours down');
                         } else {
                             notificationChannel.send('@here **⚠️ AUJOURD\'HUI : DOUBLE ILE A GOLD ⚠️**');
                         }
@@ -50,6 +52,8 @@ function reminderGoldIslandIsTomorrowOnWeekDays(client) {
                         const notificationChannel = await client.channels.cache.get(process.env.DISCORD_SERVER_NOTIFICATION_CHANNEL);
                         if (tomorrow.getDay() === 4 || tomorrow.getDay() === 6) {
                             notificationChannel.send('@here **⚠️ DEMAIN : ILE A GOLD + CHAOS GATE ⚠️**\nDouble ile à gold demain, mais également chaos gate.\nIl est préférable de faire double chaos gate');
+                        } else if (tomorrow.getDay() === 3) {
+                            notificationChannel.send('@here **⚠️ DEMAIN : ILE A GOLD + MAINTENANCE ⚠️**\nDouble ile à gold demain, mais également maintenance.\nOn parie que les serveurs ne seront pas up à 11h ?');
                         } else {
                             notificationChannel.send('@here **⚠️ DEMAIN : ILE A GOLD ⚠️**\nDouble ile à gold demain, alors ne faites pas votre ile d\'aventure aujourd\'hui');
                         }
