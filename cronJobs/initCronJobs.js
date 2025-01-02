9d566619b916eadfb860f784dd8608b957bacb3b
Baptiste Longy <baptiste.longy@gmail.com>
2025-01-02 16:41:35 +0100

    chore gvg cleanup

--- cronJobs/initCronJobs.js
+++ cronJobs/initCronJobs.js
@@ -1,6 +1,5 @@
 const { todoOfficersJob } = require('./todoOfficersJob.js');
 const { reminderJob } = require('./reminderJob.js');
-// const { reminderGVGRegistration } = require('./reminderGVGRegistration.js');
 const { reminderEventEndDate } = require('./reminderEventEndDate.js');
 const { checkLegendaryCards } = require('./checkLegendaryCards.js');
 // const { reminderGoldIslandIsToday, reminderGoldIslandIsTomorrowOnWeekDays, reminderGoldIslandIsTomorrowOnWeekEnds } = require('./reminderGoldIsland.js');
@@ -9,8 +8,6 @@ const { checkLegendaryCards } = require('./checkLegendaryCards.js');
 function initCronJobs(client) {
     todoOfficersJob(client);
     reminderJob(client);
-    // GVG disabled for now
-    // reminderGVGRegistration(client);
     reminderEventEndDate(client);
     checkLegendaryCards(client);
     // reminderGoldIslandIsToday(client);
